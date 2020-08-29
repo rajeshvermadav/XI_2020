8c704d7acee871bd6802a8215ec8124cab9e20e1
Rajesh verma <68262909+rajeshvermadav@users.noreply.github.com>
2020-08-29 12:51:00 +0530

    Add files via upload

---
+++ area_of_rectangle.py
@@ -0,0 +1,6 @@
+# calculate area of Rectangle
+length = 10
+breadth = 20
+area = length * breadth
+
+print("Area of Rectangle:", area)
---
+++ simple_interest.py
@@ -0,0 +1,8 @@
+# Calculate simple intrest
+
+p = 100
+r = 10
+t = 12
+si = (p*r*t)/100
+
+print("Simple Intrest:-", si)
